d2c8bf144b7f8d7a87369c5256caf2b2e3072038
gopalkondapally <ramgopal.k93@gmail.com>
2017-01-14 13:06:20 +0530

    Add files via upload

---
+++ src/com/inter/tests/DropDownTest.java
@@ -0,0 +1,44 @@
+package com.inter.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.inter.pages.DropDownPage;
+
+public class DropDownTest
+{
+	public WebDriver driver;
+	String exepath = "E:\\softwares\\chromedriver.exe";
+	
+	@BeforeTest
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", exepath);
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void PageHeadingTest()
+	{
+		DropDownPage ddPage = new DropDownPage(driver);
+		Assert.assertEquals(ddPage.getHeadingOfPage(), "Dropdown List");
+	}
+	
+	@Test(dependsOnMethods="PageHeadingTest")
+	public void optionSelectionTest()
+	{
+		DropDownPage ddPage = new DropDownPage(driver);
+		ddPage.selectOptionByValue(1);
+		Assert.assertEquals(ddPage.getSelectedOption(), "Option 1");
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.close();
+	}
+}
